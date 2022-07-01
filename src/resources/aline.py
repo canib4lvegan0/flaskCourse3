6c15f1090916ec0d651152f9fa9841260fb127d0
Robson Sousa <sousa.robson68@gmail.com>
2022-07-01 11:54:43 -0300

    just kidding

---
+++ src/resources/aline.py
@@ -0,0 +1,7 @@
+from flask_restful import Resource
+
+
+# noinspection PyUnreachableCode
+class Aline(Resource):
+    def get(self):
+        return {'message': 'Eu te amo, pretinha <3. Beba agua'}, 200
