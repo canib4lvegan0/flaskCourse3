3c85574caf79ad01595cf67e40d894bdd7eff09a
Robson Sousa <sousa.robson68@gmail.com>
2022-07-01 12:00:31 -0300

    Adding PostgreSQL environment variable

--- src/App.py
+++ src/App.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT, timedelta
@@ -9,7 +11,7 @@ from src.resources.store import StoreRegister, StoreId, Stores
 from src.resources.aline import Aline
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = 'robsu'
