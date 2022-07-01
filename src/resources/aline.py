120418e00c9e7ddc3537f2ac62a24d0b579b1307
Robson Sousa <sousa.robson68@gmail.com>
2022-07-01 19:49:41 -0300

    Readme

--- src/resources/aline.py
+++ src/resources/aline.py
@@ -4,4 +4,4 @@ from flask_restful import Resource
 # noinspection PyUnreachableCode
 class Aline(Resource):
     def get(self):
-        return {'message': 'Eu te amo, pretinha <3. Beba agua'}, 200
+        return {'message': 'Eu te amo, pretinha <3. Beba agua, viu?'}, 200
